b1f0955b60841444554b84d8c2b3c4e009ce168e
diamonddelt <rrrasti@yahoo.com>
2018-10-14 15:57:33 -0500

    Add shuffle method to Cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,10 @@
 defmodule Cards do
-    def hello do
-        "This is a basic function which outputs a message."
+    def create_deck do
+        ["Ace", "Two", "Three"] # implicit return of a list
+    end
+
+    # shuffle/1 = a shuffle method with an "arity" of 1
+    def shuffle(deck) do # arguments do not need a type
+        Enum.shuffle(deck) # built-in shuffle method for an enumerable (list); seems to repeat on the 1st and 2nd invokation
     end
 end
